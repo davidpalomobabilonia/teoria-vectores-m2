9001ccfc503cd18f4a52dec1ef6760894ed6e517
David <palomobabiloniadavid@gmail.com>
2024-10-24 12:05:59 -0500

    ejrcicio 3 - suma de numeros en un vector

---
+++ ejercicio-3.js
@@ -0,0 +1,14 @@
+// crea un vector con los nueros del 1 al 10, luego recojelo y suma todos los numeros del vector y muestra el resultado dela suma
+
+// definir vector de tipo numerico
+let numericos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// definir variables para almacenar la suma 
+let suma = 0;
+
+// recorrer el vector para sumar los numeros 
+for(let i = 0; i < numericos. length; i++) {suma = suma + numericos[i];}
+
+
+console.log (suma);
